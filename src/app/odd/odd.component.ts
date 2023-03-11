27e65795055174be4337e2465de16391e4bcba48
Sajid <sajid.shaikh@iceipts.com>
2023-03-11 12:23:04 +0530

    Done

---
+++ src/app/odd/odd.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-odd',
+  templateUrl: './odd.component.html',
+  styleUrls: ['./odd.component.css']
+})
+export class OddComponent {
+  // Able to pass data from outside into this component.
+  @Input() number: number;
+}
